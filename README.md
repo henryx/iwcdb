e25c6e46628829e3ff9fca12ab37c7c6ccafbde5
Enrico Bianchi <enrico.bianchi@gmail.com>
2015-10-02 00:42:35 +0200

    Added main file and README

---
+++ README.md
@@ -0,0 +1,4 @@
+Italian Web Comic Database
+==========================
+
+
---
+++ src/iwcdb.py
@@ -0,0 +1,12 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2015 Enrico Bianchi (enrico.bianchi@gmail.com)
+Project       Italian Web Comic Database
+Description   A web application for reporting information about comics
+              published in Italy
+License       GPL version 2 (see GPL.txt for details)
+"""
+
+__author__ = 'enrico'
