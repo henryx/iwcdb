943fc3ed5d17fd20f12f68199204cd3eaddfa55e
Enrico Bianchi <enrico.bianchi@gmail.com>
2015-10-06 00:07:05 +0200

    Added first tables and relations

---
+++ sql/01-tables.sql
@@ -0,0 +1,34 @@
+/*
+Copyright (C) 2015 Enrico Bianchi (enrico.bianchi@gmail.com)
+Project       Italian Web Comic Database
+Description   A web application for reporting information about comics
+              published in Italy
+License       GPL version 2 (see GPL.txt for details)
+*/
+
+CREATE TABLE lk_stato_serie(stato_serie VARCHAR(10) NOT NULL,
+                            CONSTRAINT pk_lk_stato_serie PRIMARY KEY(stato_serie)
+);
+
+CREATE TABLE lk_editore(nome_editore VARCHAR(50) NOT NULL,
+                        sede_editore VARCHAR(100),
+                        CONSTRAINT pl_lk_editore PRIMARY KEY(nome_editore)
+);
+
+CREATE TABLE lk_collana(nome_collana VARCHAR(100) NOT NULL,
+                        nome_editore VARCHAR(50),
+                        CONSTRAINT pk_lk_collana PRIMARY KEY(nome_collana)
+);
+
+CREATE TABLE lk_serie(nome_serie   VARCHAR(100) NOT NULL,
+                      nome_collana VARCHAR(100),
+                      stato_serie  VARCHAR(10),
+                      CONSTRAINT pk_lk_serie PRIMARY KEY(nome_serie)
+);
+
+CREATE TABLE ft_albo(numero_albo INTEGER,
+                     nome_serie  VARCHAR(100),
+                     data_uscita DATE,
+                     prezzo      NUMERIC(11,2),
+                     valuta      VARCHAR(3) DEFAULT 'EUR'
+);
---
+++ sql/02-relations.sql
@@ -0,0 +1,27 @@
+/*
+Copyright (C) 2015 Enrico Bianchi (enrico.bianchi@gmail.com)
+Project       Italian Web Comic Database
+Description   A web application for reporting information about comics
+              published in Italy
+License       GPL version 2 (see GPL.txt for details)
+*/
+
+ALTER TABLE lk_serie
+    ADD CONSTRAINT fk_lk_serie_1
+    FOREIGN KEY (stato_serie)
+    REFERENCES lk_stato_serie(stato_serie);
+
+ALTER TABLE lk_serie
+    ADD CONSTRAINT fk_lk_serie_2
+    FOREIGN KEY (nome_collana)
+    REFERENCES lk_collana(nome_collana);
+
+ALTER TABLE lk_collana
+    ADD CONSTRAINT fk_lk_collana_1
+    FOREIGN KEY (nome_editore)
+    REFERENCES lk_editore(nome_editore);
+
+ALTER TABLE ft_albo
+    ADD CONSTRAINT fk_ft_albo_1
+    FOREIGN KEY (nome_serie)
+    REFERENCES lk_serie(nome_serie);
