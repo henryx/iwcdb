e01a568d95c8abd34ea48be676ad6263bcebc8a7
Enrico Bianchi <enrico.bianchi@gmail.com>
2015-10-28 00:24:09 +0100

    Added package Added Python module

---
+++ src/utils/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2015 Enrico Bianchi (enrico.bianchi@gmail.com)
+Project       Italian Web Comic Database
+Description   A web application for reporting information about comics
+              published in Italy
+License       GPL version 2 (see GPL.txt for details)
+"""
+
+__author__ = 'enrico'
+
+__all__ = ["database"]
