58010f0e70de5c262fc59836595648c8fcf3a876
Enrico Bianchi <enrico.bianchi@gmail.com>
2017-05-29 23:39:02 +0200

    Updated headers

--- sql/02-relations.sql
+++ sql/02-relations.sql
@@ -3,7 +3,7 @@ Copyright (C) 2015 Enrico Bianchi (enrico.bianchi@gmail.com)
 Project       Italian Web Comic Database
 Description   A web application for reporting information about comics
               published in Italy
-License       GPL version 2 (see GPL.txt for details)
+License       GPL version 2 (see LICENSE for details)
 */
 
 ALTER TABLE lk_serie
