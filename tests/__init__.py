be7ff8a51f42435f26967decddf4039217512e65
Enrico Bianchi <enrico.bianchi@gmail.com>
2017-05-30 15:23:54 +0200

    Added unit tests

---
+++ tests/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2017 Enrico Bianchi (enrico.bianchi@gmail.com)
+Project       Italian Web Comic Database
+Description   A web application for reporting information about comics
+              published in Italy
+License       GPL version 2 (see LICENSE for details)
+"""
+
+__all__ = ["utils"]
